bfc10d699dee08892c1bfda0c86e9485b5b3f201
Alex Lowe <loweman64@outlook.com>
2021-06-16 17:09:17 -0400

    feat: Display Downloaded Images In RecyclerView

--- app/src/main/java/com/bignerdranch/andriod/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/andriod/photogallery/PhotoGalleryFragment.kt
@@ -1,8 +1,10 @@
 package com.bignerdranch.andriod.photogallery
 
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.photogallery.ThumbnailDownloader
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -23,10 +26,15 @@ class PhotoGalleryFragment : Fragment() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    retainInstance = true
+    setRetainInstance(true)
     photoGalleryViewModel = ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
-    thumbnailDownloader = ThumbnailDownloader()
-    lifecycle.addObserver(thumbnailDownloader)
+    val responseHandler = Handler()
+    thumbnailDownloader =
+      ThumbnailDownloader(responseHandler) { photoHolder, bitmap ->
+        val drawable = BitmapDrawable(resources, bitmap)
+        photoHolder.bindDrawable(drawable)
+      }
+    lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
   }
 
   override fun onCreateView(
@@ -34,6 +42,9 @@ class PhotoGalleryFragment : Fragment() {
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
+    viewLifecycleOwner.lifecycle.addObserver(
+      thumbnailDownloader.viewLifecycleObserver
+    )
     val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
 
     photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
@@ -50,11 +61,17 @@ class PhotoGalleryFragment : Fragment() {
         photoRecyclerView.adapter = PhotoAdapter(galleryItems)
       })
   }
+  override fun onDestroyView() {
+    super.onDestroyView()
+    viewLifecycleOwner.lifecycle.removeObserver(
+      thumbnailDownloader.viewLifecycleObserver
+    )
+  }
 
   override fun onDestroy() {
     super.onDestroy()
     lifecycle.removeObserver(
-      thumbnailDownloader
+      thumbnailDownloader.fragmentLifecycleObserver
     )
   }
 
