7029dd3312c7db85e8aa7e753c90d5194f74b14b
Alex Lowe <loweman64@outlook.com>
2021-06-16 15:56:41 -0400

    feat: Add Place-Holder Images to RecyclerView

--- app/src/main/java/com/bignerdranch/andriod/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/andriod/photogallery/PhotoGalleryFragment.kt
@@ -1,10 +1,13 @@
 package com.bignerdranch.andriod.photogallery
 
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -41,8 +44,37 @@ class PhotoGalleryFragment : Fragment() {
     photoGalleryViewModel.galleryItemLiveData.observe(
       viewLifecycleOwner,
       { galleryItems ->
-        Log.d(TAG, "Have gallery items from ViewModel $galleryItems")
+        photoRecyclerView.adapter = PhotoAdapter(galleryItems)
       })
+  }
+
+  private class PhotoHolder(private val itemImageView: ImageView) :
+    RecyclerView.ViewHolder(itemImageView) {
+    val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
+  }
+
+  private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
+    RecyclerView.Adapter<PhotoHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
+      val view = layoutInflater.inflate(
+        R.layout.list_item_gallery,
+        parent,
+        false
+      ) as ImageView
+      return PhotoHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+      val galleryItem = galleryItems[position]
+      val placeholder: Drawable = ContextCompat.getDrawable(
+        requireContext(),
+        R.drawable.bill_up_close
+      ) ?: ColorDrawable()
+      holder.bindDrawable(placeholder)
+    }
+
+    override fun getItemCount(): Int = galleryItems.size
+
   }
 
   companion object {
